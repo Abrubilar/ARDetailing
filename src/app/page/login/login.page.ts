5de3d34f658966aa77085d7df113ece55303a654
Abraham R. <ab.rubilar@duocuc.cl>
2023-09-08 21:42:32 -0300

    se agrega funcion login

--- src/app/page/login/login.page.ts
+++ src/app/page/login/login.page.ts
@@ -7,9 +7,24 @@ import { Component, OnInit } from '@angular/core';
 })
 export class LoginPage implements OnInit {
 
+  formLogin = {
+    rut: "",
+    password: ""
+  }
+
+  rut: string = ""
+  password: string = ""
+
+
   constructor() { }
 
   ngOnInit() {
+  }
+
+  iniciarSesion()
+  {
+    console.log("rut"+this.formLogin.rut);
+    console.log("password"+this.formLogin.password);
   }
 
 }
